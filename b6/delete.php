9bda54eafe1f27215f44a8bf4624278020637e16
Onner04 <Onner04>
2024-09-25 20:25:19 +0700

    update

--- b6/delete.php
+++ b6/delete.php
@@ -4,15 +4,15 @@ $username = "root";
 $password = "";
 $dbname = "b5_mydb";
 
-// Tạo kết nối
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Câu lệnh SQL để xóa dữ liệu
+
 $sql = "DELETE FROM MyGuests WHERE id=3";
 
 if ($conn->query($sql) === TRUE) {
